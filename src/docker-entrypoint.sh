c1060ecf3d3496ede551b27ab5fd58486d714cb0
Maxim <lmaokeklol@gmail.com>
2023-02-02 20:53:35 +0300

    Refactor docker files

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Collect static files
-echo "Collect static files"
-python manage.py collectstatic --noinput
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-# Apply database migrations
-echo "Apply database migrations"
-python3 manage.py migrate
+    while ! nc -vz "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+      sleep 0.15
+    done
 
-# Start server
-echo "Starting server"
-gunicorn culinary.wsgi:application --bind 0.0.0.0:8000 --workers 3
+    echo "PostgreSQL started"
+fi
+
+
+
+echo "[MIGRATE]"
+python manage.py migrate --no-input
+
+echo "[COLLECT STATIC]"
+python manage.py collectstatic --no-input
+
+echo "[RUN SERVER]"
+gunicorn culinary.wsgi:application --bind 0.0.0.0:8002
